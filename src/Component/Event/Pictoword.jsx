2642bde62f87df58368cd865a3a7f4eb742d2c3a
SaaiThirumu14 <148644164+SaaiThirumu14@users.noreply.github.com>
2025-03-27 19:47:07 +0530

    Update Pictoword.jsx

--- src/Component/Event/Pictoword.jsx
+++ src/Component/Event/Pictoword.jsx
@@ -7,6 +7,7 @@ import image from "../Event/images/Pictoword.webp";
 
 export default function Pictoword() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isloading, setisloading] = useState(false);
   const [teamLimitReached, setTeamLimitReached] = useState(false);
   const [formData, setFormData] = useState({
     teamName: "",
@@ -113,6 +114,7 @@ export default function Pictoword() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setisloading(true)
 
     try {
       const res = await axios.post(
@@ -148,7 +150,7 @@ export default function Pictoword() {
         });
         setIsOpen(false);
         checkTeamLimit();
-      } else if (res.status === 409) {
+      } else if (res.status === 201) {
         toast.error("Team name already exist");
       } else {
         toast.error("Registration failed.");
@@ -157,6 +159,7 @@ export default function Pictoword() {
       console.error("Registration error:", err);
       toast.error(err.response?.data || "Something went wrong.");
     }
+    setisloading(false)
   };
 
   return (
@@ -494,11 +497,16 @@ export default function Pictoword() {
               </select>
 
               <div className="flex justify-center">
-                <button
+              <button
                   type="submit"
-                  className="bg-purple-600 hover:bg-purple-700 text-white py-2 px-6 rounded-lg shadow-[0_0_12px_3px_rgba(168,85,247,0.5)] transition-all"
+                  className={`w-full py-3 text-white rounded-md text-md font-semibold transition-all ${
+                    isloading
+                      ? "bg-gray-500 cursor-not-allowed"
+                      : "bg-purple-600 hover:bg-purple-700"
+                  }`}
+                  disabled={isloading}
                 >
-                  Submit
+                  {isloading ? "Submitting..." : "Register"}
                 </button>
               </div>
             </form>
